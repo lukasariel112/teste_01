cecaa2a2049173d0043a24da9a6d1880918bb25d
lukasariel112 <lluvas99@gmail.com>
2023-07-10 21:36:30 -0300

    boa noite

--- teste.js
+++ teste.js
@@ -2,4 +2,12 @@ function aplicardesconto(valor, desconto)
 {
     return valor - desconto
 }
+function validarnumero(valor)
+{
+    return isNaN(valor)
+}
 console.log ('APP funcionando')
+console.log ('aplicardesconto()')
+
+module.exports ={aplicardesconto}
+module.exports ={validarnumero}
